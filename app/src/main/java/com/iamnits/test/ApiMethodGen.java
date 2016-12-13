0fdc998b3ac8b8c185e5af54665104140277989d
iamnitish <nitish0390@gmail.com>
2016-12-13 12:53:45 +0530

    first commit

---
+++ app/src/main/java/com/iamnits/test/ApiMethodGen.java
@@ -0,0 +1,20 @@
+package com.iamnits.test;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+/**
+ * App Api Interface , methods
+ */
+public interface ApiMethodGen {
+    /**
+     * Return restaurant data on page wise api call
+     * @param page
+     * @return restaurant data as response
+     */
+    //https://api.myjson.com/bins/ngcc?page=0
+    @GET(HttpRequestMaker.domain+"/bins/ngcc?")
+    Call<RestaurantDataModel> getRestaurantData(@Query("page") int page);
+
+}
